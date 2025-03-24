d14207336bccf7bb2e395062791e3f2b9f16a24e
leon <gzapata9804@gmail..com>
2025-03-24 15:13:58 -0600

    actualizacion de login

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,16 +2,32 @@ import { createVuetify } from 'vuetify'
 import '@mdi/font/css/materialdesignicons.css'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin, useHead } from '#app'
 import 'vuetify/styles'
 
 export default defineNuxtPlugin((nuxtApp) => {
+  useHead({
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700&display=swap'
+      }
+    ]
+  })
+
   const vuetify = createVuetify({
     components,
     directives,
     icons: {
-      defaultSet: 'mdi', 
-    }
+      defaultSet: 'mdi',
+    },
+    defaults: {
+      VApp: {
+        style: {
+          fontFamily: 'Nunito Sans, sans-serif'
+        }
+      }
+    },
   })
 
   nuxtApp.vueApp.use(vuetify)
