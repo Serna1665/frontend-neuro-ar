6498ef24b1755867dbc9d295ee2a99b7ce2bdb3b
serna <cristianserna1606@gmail.com>
2025-03-24 21:05:49 -0500

    cambios en el panel de usuarios

---
+++ components/Usuarios/editarUsuarioComponente.vue
@@ -0,0 +1,104 @@
+<template>
+    <div>
+        <v-card :loading="loading" :disabled="loading">
+            <v-card-title>
+                <div class="d-flex align-center justify-space-between w-100">
+                    <div class="d-flex align-center">
+                        <img src="/public/imagenes/icono-logo.png" alt="Logo" height="70px" width="70px">
+                        <v-divider inset vertical class="ml-6"></v-divider>
+                        <strong class="ml-4 text-h5 font-weight-bold">Editar Usuario # {{datosUsuarios?.id}}</strong>
+                    </div>
+                </div>
+            </v-card-title>
+            <v-card-text>
+                <v-row>
+                    <v-col cols="4">
+                        <v-text-field label="Nombres" readonly density="comfortable" variant="outlined"
+                            v-model="datosPaciente.nombres">
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-text-field label="Apellidos" readonly density="comfortable" variant="outlined"
+                            v-model="datosPaciente.apellidos">
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-text-field label="Género" readonly density="comfortable" variant="outlined"
+                            v-model="datosPaciente.genero">
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-autocomplete label="Oficio" variant="outlined" :items="oficios" item-title="nombre"
+                            item-value="id" v-model="datosPaciente.oficio_id" density="comfortable">
+                        </v-autocomplete>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-text-field label="Fecha de nacimiento" readonly density="comfortable" variant="outlined"
+                            v-model="datosPaciente.fecha_nacimiento">
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-autocomplete  :items="['Diestro', 'Zurdo', 'Ambiestro']" label="Lateralidad dominante" density="comfortable" variant="outlined"
+                            v-model="datosPaciente.lateralidad_dominante">
+                        </v-autocomplete>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-text-field label="Estatura" type="number" density="comfortable" variant="outlined"
+                            v-model="datosPaciente.estatura">
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-text-field label="Email" density="comfortable" variant="outlined"
+                            v-model="datosUsuarios.email">
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-text-field type="number" label="Número de documento" density="comfortable" variant="outlined"
+                            v-model="datosUsuarios.numero_documento">
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['datosUsuarios'],
+        data() {
+            return {
+                datosPaciente: [],
+                oficios: [],
+                loading: false
+            }
+        },
+
+        mounted() {
+            this.listarOficios()
+        },
+
+        methods: {
+            obtenerDatosUsuario() {
+                this.loading = true;
+                this.$axios.get('pacientes/datos-paciente/' + this.datosUsuarios.id).then((res) => {
+                    this.datosPaciente = res.data
+                }).catch((error) => {
+                    console.log(error.response.data.error)
+                }).finally(() => {
+                    this.loading = false
+                })
+            },
+
+            async listarOficios() {
+                try {
+                    const response = await this.$axios.get('oficios/listar');
+                    this.oficios = response.data;
+                } catch {
+                    console.error('Error al obtener los oficios');
+                }
+            },
+
+        }
+    }
+</script>
--- pages/usuarios.vue
+++ pages/usuarios.vue
@@ -1,3 +1,137 @@
 <template>
-    <div><h1>slkdsl</h1></div>
+    <div>
+        <v-card>
+            <v-card-title>
+                <div class="d-flex align-center justify-space-between w-100">
+                    <div class="d-flex align-center">
+                        <img src="/public/imagenes/icono-logo.png" alt="Logo" height="70px" width="70px">
+                        <v-divider inset vertical class="ml-6"></v-divider>
+                        <strong class="ml-4 text-h5 font-weight-bold">Usuarios</strong>
+                    </div>
+                </div>
+            </v-card-title>
+
+            <v-container fluid>
+                <v-row>
+                    <v-col cols="4">
+                        <v-text-field label="Correo electronico" density="comfortable" variant="outlined"
+                            v-model="buscar.email">
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-text-field label="Número de documento" density="comfortable" variant="outlined"
+                            v-model="buscar.documento">
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-text-field label="Nombre del usuario" density="comfortable" variant="outlined"
+                            v-model="buscar.nombre">
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+                <v-col>
+                    <v-btn color="primary" @click="buscarUsuario()" :loading="loading">Filtrar usuario</v-btn>
+                </v-col>
+            </v-container>
+
+            <v-col cols="12" v-if="usuarios.length > 0">
+                <v-data-table no-data-text="Sin usuarios registrados" :headers="headers" :items="usuarios">
+                    <template v-slot:[`item.created_at`]="{ item }">
+                        <div>
+                            {{item.created_at?.substring(0,10)}}
+                        </div>
+                    </template>
+
+                    <template v-slot:[`item.acciones`]="{ item }">
+                        <v-tooltip location="top">
+                            <template v-slot:activator="{ props }">
+                                <v-icon v-bind="props" color="warning" icon="mdi-pencil" size="small"
+                                    @click="abrirDialogEditarUsuario(item)">
+                                </v-icon>
+                            </template>
+                            <span>Editar usuario</span>
+                        </v-tooltip>
+                    </template>
+                </v-data-table>
+            </v-col>
+        </v-card>
+
+        <v-dialog v-model="dialogEditarUsuario">
+            <editarUsuario ref="editarUsuarioRef" :datosUsuarios="datosUsuarios"></editarUsuario>
+        </v-dialog>
+    </div>
 </template>
+
+<script>
+import editarUsuario from "@/components/Usuarios/editarUsuarioComponente.vue";
+    export default {
+        components: {
+            editarUsuario
+        },
+        middleware: 'auth',
+        data() {
+            return {
+                buscar: {
+                    email: '',
+                    documento: '',
+                    nombre: '',
+                },
+                usuarios: [],
+                loading: false,
+                dialogEditarUsuario: false, 
+                datosUsuarios: {},
+                headers: [{
+                        title: 'ID',
+                        key: 'id'
+                    },
+                    {
+                        title: 'Nombre',
+                        key: 'name'
+                    },
+                    {
+                        title: 'Email',
+                        key: 'email'
+                    },
+                    {
+                        title: 'Número de documento',
+                        key: 'numero_documento'
+                    },
+                    {
+                        title: 'Fecha de creación',
+                        key: 'created_at'
+                    },
+                    {
+                        title: 'Acciones',
+                        key: 'acciones'
+                    }
+                ]
+            }
+        },
+
+        methods: {
+            buscarUsuario() {
+                const params = {
+                    ...this.buscar
+                }
+                this.loading = true
+                this.$axios.get('pacientes/buscar', {
+                    params
+                }).then((res) => {
+                    this.usuarios = res.data
+                }).catch((error) => {
+                    console.log(error.response.data.error)
+                }).finally(() => {
+                    this.loading = false
+                })
+            },
+
+            abrirDialogEditarUsuario(item) {
+                this.datosUsuarios = item;
+                this.dialogEditarUsuario = true
+                this.$nextTick(() => {
+                    this.$refs.editarUsuarioRef.obtenerDatosUsuario()
+                })
+            }
+        }
+    }
+</script>
